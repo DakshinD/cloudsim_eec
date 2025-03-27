306bfbb90d83e999d07537b481552821344238db
Dakshin Devanand <dakshind2005@gmail.com>
2025-03-26 22:55:12 -0500

    Started migration - assertion bug found. applied patches from TAs

--- Scheduler.cpp
+++ Scheduler.cpp
@@ -5,13 +5,6 @@
 //  Created by ELMOOTAZBELLAH ELNOZAHY on 10/20/24.
 //
 
-/*
-    TO-DO/Thoughts:
-    - Fix starvation problem
-    - use burst property again, check if sys overloaded, if it is, increase min percentage, and turn 
-      bunch machines on
-*/
-
 #include "Scheduler.hpp"
 #include <map>
 #include <set>
@@ -35,6 +28,7 @@ struct MachineState {
     Time_t last_state_change;
 };
 
+const MachineState_t SLEEP_STATE = S0i1;
 const bool PROGRESS_BAR = true;
 const bool MACHINE_STATE = false;
 const bool TEST = false;
@@ -49,6 +43,7 @@ unsigned total_on_machines = -1;
 map<CPUType_t, unsigned> on_cpu_count;
 map<MachineState_t, unsigned> state_count;
 
+vector<MachineId_t> machines;
 map<TaskId_t, VMId_t> task_assignments;
 vector<VMId_t> vms;
 map<MachineId_t, MachineState> machine_states;
@@ -58,6 +53,123 @@ map<VMId_t, MachineId_t> ongoing_migrations;
 // Reporting Data Structures
 int total_sla[NUM_SLAS] = {0};
 int sla_violations[NUM_SLAS] = {0};
+
+// Energy DS
+struct EnergyState {
+    double energy_consumption_rate;
+    uint64_t prev_energy_consumption;
+    Time_t prev_time;
+};
+
+map<MachineId_t, EnergyState> machine_energy_rates;
+const double UNDERUTIL_THRESHOLD = 2.0;
+const double OVERUTIL_THRESHOLD = 8.0;
+
+
+// Func headers
+MachineId_t GetBestMachine(TaskId_t task_id);
+void MigrateHelper(VMId_t vm_id, MachineId_t start_m, MachineId_t end_m);
+void Debug();
+void DebugVM(VMId_t vm_id);
+/*
+
+
+NewTask - 4.1 VM Placement
+
+- Each Task going on its own VM? Seems to be the case
+- Calculate current rate of energy consumption for each machine
+    a. Keep DS that stores for each machine, prev energy consumption, prev rate, and prev time
+    b. In PeriodicCheck, calculate the new rate and update prev energy and prev rate
+
+    
+
+Migration Policy (Also done in PeriodicCheck)
+- We set a lower and upper threshold for under/overutilized machines
+- We need a method to calculate utilization (cores, tasks, VMs, memory)
+1. Underutilized
+- Migrate all VMs off
+2. Overutilized
+- while the current machine is above the threshold
+    a. migrate off the VM with lowest priority task
+    b. in future, combine remaining instructions, duration left, and priority to find optimal machine to migrate 
+*/
+
+/*
+    We need a method to calculate utilization (cores, tasks, VMs, memory)
+*/
+double MachineUtilization (MachineId_t machine_id) {
+    MachineInfo_t machine_info = Machine_GetInfo(machine_id);
+
+    const double W_CORE = 3.0;
+    const double W_MEM = 1.0;
+
+    // Calculate utilization based on cores, tasks, VMs, and memory
+    double core_utilization = (double)machine_info.active_vms / machine_info.num_cpus;
+    double memory_utilization = (double)machine_info.memory_used / machine_info.memory_size;
+    
+    // Combine the utilization metrics into a single score
+    double utilization_score = ((W_CORE * core_utilization) + (W_MEM * memory_utilization)) / 2.0;
+
+    // Normalize the score to a range of 0 to 1
+    return std::clamp(utilization_score, 0.0, 10.0);
+}
+
+void LoadBalance() {
+    for (auto& [machine_id, m_state] : machine_states) {
+        if (m_state.state != ON) continue;
+
+        MachineInfo_t m_info = Machine_GetInfo (machine_id);
+        double machine_util = MachineUtilization (machine_id);
+        // Under
+        if (machine_util < UNDERUTIL_THRESHOLD) {
+            // We need to migrate all VMs off this machine
+            assert(m_state.vms.size() == m_info.active_vms);
+            // printf("Migrating %d VMs\n", m_state.vms.size());
+
+            set<VMId_t> need_to_migrate = m_state.vms;
+            for (const auto& vm_id : need_to_migrate) {
+                // Get best machine
+                MachineId_t dest_machine = GetBestMachine(VM_GetInfo(vm_id).active_tasks[0]); // Can only do this because there is 1 task per VM
+                if (dest_machine == machine_id) {
+                    // ThrowException("Migrating to same machine");
+                    continue; // NEED TO FIX
+                }
+
+                // If we couldn't find an ON machine to migrate to, just give up
+                if (machine_states[dest_machine].state != ON) {
+                    break;
+                }
+
+                MigrateHelper(vm_id, machine_id, dest_machine); 
+            }
+            m_info = Machine_GetInfo(machine_id); 
+            // if (m_state.vms.size() != m_info.active_vms) {
+            //     // printf("%d\n", Now());
+            //     Debug();
+            //     printf("VM size: %d vs active_vms: %d\n", m_state.vms.size(), m_info.active_vms);
+            // }
+            // assert(m_state.vms.size() == m_info.active_vms);
+            
+
+            // See if we can turn this machine off
+            if (m_info.active_vms == 0 && on_cpu_count[m_info.cpu] > 2) { // Keep at least 2 machines of each CPU type on
+                Machine_SetState (machine_id, SLEEP_STATE);
+                m_state.state = TURNING_OFF;
+                total_on_machines--;
+                on_cpu_count[Machine_GetCPUType(machine_id)]--;
+            }
+        }
+        
+        // Over
+        // if (machine_util > OVERUTIL_THRESHOLD) {
+        //     for (auto& vm_id : m_state.vms) {
+        //         MigrateHelper(vm_id, machine_id, GetBestMachine(VM_GetInfo(vm_id).active_tasks[0]));
+        //         break;
+        //     }
+        // }
+    }
+}
+
 
 void DebugVM(VMId_t vm_id) {
     VMInfo_t vm_info = VM_GetInfo(vm_id);
@@ -99,7 +211,7 @@ void Debug() {
     string res = "DETAILED MACHINE TO VMs BREAKDOWN:\n";
     for (const auto& [machine_id, m_state] : machine_states) {
         MachineInfo_t machine_info = Machine_GetInfo(machine_id);
-        if (machine_info.active_tasks == 0 && pending_attachments[machine_id].size() == 0) continue;
+        if (machine_info.active_tasks == 0 && machine_info.active_vms == 0 && pending_attachments[machine_id].size() == 0) continue;
         // if (m_state.vms.size() != machine_info.active_vms) ThrowException("Machine " + to_string(machine_id) + " has " + to_string(m_state.vms.size()) + " VMs but " + to_string(machine_info.active_vms) + " active VMs");
         res += "\033[1;35mMachine " + to_string(machine_id) + " (" + to_string(m_state.vms.size()) + " VMs / " + to_string(machine_info.num_cpus) + " CPUs):\033[0m (" + "\033[1;34m" + to_string(pending_attachments[machine_id].size()) + "\033[0m) " +
                "[\033[1;36mS-State: " + to_string(machine_info.s_state) + "\033[0m, \033[1;33mPower-State: " + (m_state.state == ON ? "ON" : m_state.state == TURNING_ON ? "TURNING_ON" : m_state.state == TURNING_OFF ? "TURNING_OFF" : "OFF") + "\033[0m]\n";
@@ -128,81 +240,30 @@ void Debug() {
 
 
 /*
-    We need to choose a VM based on priority, but also CPU type and GPU, have a VM have same CPU/GPU type 
+    Add task to an on or off machine
 */
 void Add_TaskToMachine(MachineId_t machine_id, TaskId_t task_id) {
-    // Choose whether to create VM or add to existing
-    VMId_t vm_id;
-    bool created_new_vm = false;
-    TaskInfo_t task_info = GetTaskInfo(task_id);
-    Priority_t task_priority = task_info.priority;
+    // If the machine is ON
+    if (machine_states[machine_id].state == ON) {
+        VMId_t vm_id = VM_Create(RequiredVMType(task_id), RequiredCPUType(task_id));
+        TaskInfo_t task_info = GetTaskInfo(task_id);
+        Priority_t task_priority = task_info.priority;
 
-    /* 
-        1. Create a new VM when
-            a. # of VMs < # of CPUs
-            b. Load balancing 
-        2. Add to existing VM
-            a. # of VMs >= # of CPUs
-    */
-
-    // If # of VMs < # of CPUs, create a new VM
-    if (machine_states[machine_id].vms.size() < Machine_GetInfo(machine_id).num_cpus) {
-        vm_id = VM_Create(RequiredVMType(task_id), RequiredCPUType(task_id));
-        created_new_vm = true;
-    } else {
-        // Find an existing VM to add the task to TBD: Needs to be the required VM Type
-        VMId_t best_vm_id = -1;
-        double best_score = __DBL_MAX__;
-        for (const auto& vm : machine_states[machine_id].vms) {
-            VMInfo_t vm_info = VM_GetInfo(vm);
-            if (RequiredVMType(task_id) != vm_info.vm_type) continue;
-
-            double score = 0.0;
-            switch (task_priority) {
-                case HIGH_PRIORITY:
-                    // For high priority, find VM with least high and mid priority tasks
-                    score += std::count_if(vm_info.active_tasks.begin(), vm_info.active_tasks.end(), [](TaskId_t t) {
-                        Priority_t p = GetTaskInfo(t).priority;
-                        return p == HIGH_PRIORITY;
-                    });
-                    break;
-                case MID_PRIORITY:
-                    // For medium priority, find VM with least amount of high + medium priority tasks
-                    score += std::count_if(vm_info.active_tasks.begin(), vm_info.active_tasks.end(), [](TaskId_t t) {
-                        Priority_t p = GetTaskInfo(t).priority;
-                        return p == HIGH_PRIORITY || p == MID_PRIORITY;
-                    });
-                    
-                    break;
-                case LOW_PRIORITY:
-                    // For low priority, find VM with least total tasks
-                    score = vm_info.active_tasks.size();
-                    break;
-            }
-            // Update our best vm
-            if (score < best_score) {
-                best_score = score;
-                best_vm_id = vm;
-            }
-        }
-        if (best_vm_id != -1) {
-            vm_id = best_vm_id;
-        } else {
-            // If no suitable VM found, create a new one
-            vm_id = VM_Create(RequiredVMType(task_id), RequiredCPUType(task_id));
-            created_new_vm = true;
-        }
-    }
-    if (created_new_vm) {
         VM_Attach(vm_id, machine_id);
-        int prev_sz = machine_states[machine_id].vms.size();
         machine_states[machine_id].vms.insert(vm_id);
-        assert(prev_sz + 1 == machine_states[machine_id].vms.size());
-    } 
-    VM_AddTask(vm_id, task_id, Priority_t(GetTaskPriority(task_id)));
-    task_assignments[task_id] = vm_id;
-    SimOutput("NewTask(): Added " + to_string(task_id) + " on vm: " + to_string(vm_id) + " to on machine " + to_string(machine_id), 1);
 
+        VM_AddTask(vm_id, task_id, Priority_t(GetTaskPriority(task_id)));
+        task_assignments[task_id] = vm_id;
+        SimOutput("Add_TaskToMachine(): Added " + to_string(task_id) + " on vm: " + to_string(vm_id) + " to on machine " + to_string(machine_id), 1);
+    } else if (machine_states[machine_id].state == OFF) {
+    // If the machine is not ON
+        Machine_SetState(machine_id, S0);
+        machine_states[machine_id].state = TURNING_ON;
+        pending_attachments[machine_id].push_back(task_id);
+        SimOutput("Add_TaskToMachine(): Turning on machine " + to_string(machine_id) + " for task " + to_string(task_id), 1);
+    } else {
+        ThrowException("Tried adding task to machine not ON or OFF");
+    }
     return;
 }
 
@@ -223,15 +284,14 @@ void Scheduler::Init() {
     total_tasks = GetNumTasks();
     for(unsigned i = 0; i < total_machines; i++) {
         machines.push_back(MachineId_t(i));
+        machine_energy_rates[i] = {0, 0, Now()};
         machine_states[i] = {{}, ON, Now()};
         on_cpu_count[Machine_GetCPUType(MachineId_t(i))]++;
         state_count[S0]++;
-        // Set the default state of all machines to sleeping for greedy
-        // Machine_SetState (MachineId_t(i), S0);
+        
         SimOutput("Scheduler::Init(): Created machine id of " + to_string(i), 4);
     }    
 
-    SLEEP_STATE = burst_tracker.current_sleep_state;
 }
 
 void Scheduler::MigrationComplete(Time_t time, VMId_t vm_id) {
@@ -253,73 +313,59 @@ void Scheduler::MigrationComplete(Time_t time, VMId_t vm_id) {
 }
 
 /*
-Use objective function to find machine and add it according to machine state (ON/OFF)
+    Can return either an ON or OFF machine. However, if returning an OFF machine, it DOES NOT turn it on for you.
 */
-void Scheduler::NewTask(Time_t now, TaskId_t task_id) {
-    burst_tracker.recordTask();
-    if (burst_tracker.updateBurstStatus(now)) {
-        // get the new sleep state from burst tracker
-        SLEEP_STATE = burst_tracker.current_sleep_state;
-        
-        // log change
-        string message = burst_tracker.in_burst ? 
-            "Burst started! Changing sleep state to S0i1" :
-            "Burst ended! Changing sleep state back to S1";
-        SimOutput(message + " (Task count: " + 
-                 to_string(burst_tracker.task_count) + ")", 0);
+MachineId_t GetBestMachine(TaskId_t task_id) {
+    TaskInfo_t t_info = GetTaskInfo(task_id);
+    vector<MachineId_t> sorted_machines = machines;
 
-        for (auto& [machine_id, m_state] : machine_states) {
-            if (m_state.state == OFF) {
-                MachineInfo_t m_info = Machine_GetInfo(machine_id);
-                if (m_info.s_state > SLEEP_STATE) {
-                    state_count[m_info.s_state]--;
-                    Machine_SetState(machine_id, SLEEP_STATE);
-                    machine_states[machine_id].state = TURNING_OFF; 
-                }
-            }
+    std::sort(sorted_machines.begin(), sorted_machines.end(), [](MachineId_t a, MachineId_t b) {
+        return machine_energy_rates[a].energy_consumption_rate < machine_energy_rates[b].energy_consumption_rate;
+    });
+
+    // Iterate through sorted machines to find a suitable one
+    for (const auto& machine_id : sorted_machines) {
+        MachineInfo_t m_info = Machine_GetInfo(machine_id);
+
+        // Check if the machine is ON and is compatible
+        if (machine_states[machine_id].state != ON || t_info.required_cpu != m_info.cpu) {
+            continue;
         }
-    }
 
-    SimOutput("NewTask(): New task at time: " + to_string(now), 1);
-    // Get the task parameters
-    TaskInfo_t task = GetTaskInfo (task_id);
-
-    // Set the task priority based on its SLA
-    // if (task.required_sla == SLA0) {
-    //     SetTaskPriority(task_id, HIGH_PRIORITY);
-    // } else if (task.required_sla == SLA1 || task.required_sla == SLA2) {
-    //     SetTaskPriority(task_id, MID_PRIORITY);
-    // } else if (task.required_sla == SLA3) {
-    //     SetTaskPriority(task_id, LOW_PRIORITY);
-    // }
-
-    // Keep track of best matched machine
-    MachineId_t best_machine_id = GetBestScoreMachine(task_id); 
-
-    if (best_machine_id == -1) {
-        ThrowException("Scheduler::NewTask(): Couldn't find a machine for task " + to_string(task_id));
-    }
-
-    // We have a machine, lets check its state
-    MachineState target = machine_states[best_machine_id];
-    if (target.state == ON) {
-        Add_TaskToMachine(best_machine_id, task_id);
-    }
-    else if (target.state == OFF || target.state == TURNING_ON) {
-        if (target.state == OFF) {
-            state_count[Machine_GetInfo(best_machine_id).s_state]--;
-            Machine_SetState(best_machine_id, S0);
-            machine_states[best_machine_id].state = TURNING_ON;
-        }
-        pending_attachments[best_machine_id].push_back(task_id);
-        SimOutput("NewTask(): Added PENDING " + to_string(task_id) + " to off machine " + to_string(best_machine_id), 1); 
-    } else {
-        // state == TURNING_OFF
-        // Question: What happens if we setstate to S0 while it is turning off?
-        pending_attachments[best_machine_id].push_back(task_id);
-        SimOutput("NewTask(): Added PENDING " + to_string(task_id) + " to turning off machine " + to_string(best_machine_id), 1); 
+        // place task into machine
+        return machine_id;
     }
     
+    // Find an off machine
+    for (const auto& [machine_id, m_state] : machine_states) {
+        if (m_state.state == OFF) {
+            return machine_id;
+        }
+    }
+
+    // Should never get this?
+    return -1;
+}
+
+/*
+- For NewTask, 
+    1. sort the machines in ascending energy consumption rates
+    2. check if machine has enough resources/compatible
+*/
+void Scheduler::NewTask(Time_t now, TaskId_t task_id) {
+    TaskInfo_t t_info = GetTaskInfo(task_id);
+    // sort machines by energy consumption rate
+
+    MachineId_t best_machine = GetBestMachine(task_id);
+    if (best_machine != -1) {
+        Add_TaskToMachine(best_machine, task_id);
+        return;
+    }
+
+    // OOPS
+    SimOutput("NewTask(): No suitable machine found for task " + to_string(task_id), 1);
+    Debug();
+    ThrowException("fuck");
     return;
 }
 
@@ -368,8 +414,7 @@ void MigrateHelper(VMId_t vm_id, MachineId_t start_m, MachineId_t end_m) {
 
 
 /*
-    Idea: We could track the last instruction we were at for that task, and then if it seems that we aren't on track for completion,
-    load balance it or up the priority
+    Runs every 60 ms or 60,000 microseconds
 */
 void Scheduler::PeriodicCheck(Time_t now) {
     if (PROGRESS_BAR) {
@@ -378,6 +423,17 @@ void Scheduler::PeriodicCheck(Time_t now) {
     if (MACHINE_STATE) {
         DisplayMachineStates();
     }
+    // Calculate energy rates of all machines
+    for (auto& [machine_id, e_state] : machine_energy_rates) {
+        uint64_t cur_consumption = Machine_GetEnergy(machine_id);
+        e_state.energy_consumption_rate = (cur_consumption - e_state.prev_energy_consumption)/(now - e_state.prev_time);
+        e_state.prev_energy_consumption = cur_consumption;
+        e_state.prev_time = now;
+    }  
+    
+    // Check for under and over utilized machines
+    LoadBalance();
+    
 }
 
 void Scheduler::Shutdown(Time_t time) {
@@ -394,9 +450,9 @@ void Scheduler::TaskComplete(Time_t now, TaskId_t task_id) {
     VMInfo_t vm_info = VM_GetInfo(vm_id);
     SimOutput("Scheduler::TaskComplete(): Task " + to_string(task_id) + " is complete at " + to_string(now) + " on vm " + to_string(vm_id), 1);
     
-    // if (VM_GetInfo(vm_id).active_tasks.size() != 0) {
-    //     ThrowException("Somehow there are active tasks on a VM?????");
-    // }
+    if (VM_GetInfo(vm_id).active_tasks.size() != 0) {
+        ThrowException("Somehow there are active tasks on a VM?????");
+    }
 
     // PROGRESS
     completed_tasks++;
@@ -412,7 +468,7 @@ void Scheduler::TaskComplete(Time_t now, TaskId_t task_id) {
         VM_Shutdown(vm_id);
         machine_states[orig_m_id].vms.erase(vm_id);
     }
-    assert(machine_states[orig_m_id].vms.size() == Machine_GetInfo(orig_m_id).active_vms); 
+    // assert(machine_states[orig_m_id].vms.size() == Machine_GetInfo(orig_m_id).active_vms); 
 }
 
 // Public interface below
@@ -520,8 +576,6 @@ void SLAWarning(Time_t time, TaskId_t task_id) {
         ThrowException("SLA Violation for task " + to_string(task_id) + " on machine " + to_string(VM_GetInfo(task_assignments[task_id]).machine_id) + " at time " + to_string(time));
     }
 
-    // What do we do if we identify tasks on the same machine that are going to violate SLA?
-    // Starvation is occurring of SLA1 tasks because SLA0 is so spread out
 }
 
 void StateChangeComplete(Time_t time, MachineId_t machine_id) {
@@ -546,7 +600,6 @@ void StateChangeComplete(Time_t time, MachineId_t machine_id) {
             }
             pending_attachments.erase(machine_id);
         }
-        // Debug();
     } else {
         // If we turned it off
         machine_states[machine_id].state = OFF; 
